fcf408f17e86f5f1a405e50fe81565e6ed3e015b
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-12 17:59:45 -0300

    chore: create BaseEntity and Product Entity

---
+++ src/Services/Catalog/Catalog.Api/Entities/BaseEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Api.Entities
+{
+    public class BaseEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; protected set; }
+        
+        private DateTime? _createdAt;
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime? CreatedAt
+        {
+            get => _createdAt ?? DateTime.UtcNow;
+            set => _createdAt = value;
+        }
+        
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}

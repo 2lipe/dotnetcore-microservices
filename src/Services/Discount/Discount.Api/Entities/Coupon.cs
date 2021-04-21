558200fde612443ff1aaf7990bb80e1f2e7fe105
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-21 18:27:44 -0300

    chore: create Coupon entity

---
+++ src/Services/Discount/Discount.Api/Entities/Coupon.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Discount.Api.Entities
+{
+    public class Coupon
+    {
+        public Guid Id { get; set; }
+        
+        public string ProductName { get; set; }
+        
+        public string Description { get; set; }
+        
+        public int Amount { get; set; }
+    }
+}
